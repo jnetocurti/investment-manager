48667787af120d9a04d53f3e95aa3cc2c73f857f
Jose Neto <jose.neto@riachuelo.com.br>
2024-01-07 14:46:23 -0300

    feat: mongo

--- src/main/java/br/com/investmentmanager/tradingnote/integration/mongo/TradingNoteRepositoryImpl.java
+++ src/main/java/br/com/investmentmanager/tradingnote/integration/mongo/TradingNoteRepositoryImpl.java
@@ -2,12 +2,18 @@ package br.com.investmentmanager.tradingnote.integration.mongo;
 
 import br.com.investmentmanager.tradingnote.domain.aggregate.TradingNote;
 import br.com.investmentmanager.tradingnote.domain.repository.TradingNoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class TradingNoteRepositoryImpl implements TradingNoteRepository {
+
+    private final MongoRepository<TradingNote, String> mongoRepository;
 
     @Override
     public void save(TradingNote tradingNote) {
+        mongoRepository.save(tradingNote);
     }
 }
